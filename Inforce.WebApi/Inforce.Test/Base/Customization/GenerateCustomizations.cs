cb7f3bb93f26ce69ca7027897ee69b4948e7ee66
Mark14444 <mih.mark2015@gmail.com>
2022-10-05 11:31:05 +0300

    Added customization for auto data

---
+++ Inforce.WebApi/Inforce.Test/Base/Customization/GenerateCustomizations.cs
@@ -0,0 +1,41 @@
+﻿using AutoFixture;
+using Inforce.Service.Dto.AuthorizationDtos;
+using Inforce.Service.Dto.UrlDtos;
+using Inforce.Service.Dto.UserDtos;
+using System.Net.Mail;
+
+namespace Fragments.Test.Base.Customization
+{
+    public class GenerateCustomizations : ICustomization
+    {
+        public void Customize(IFixture fixture)
+        {
+            fixture.Customize<CreateUserDto>(composer =>
+            composer
+            .With(x =>
+            x.Email,
+            fixture.Create<MailAddress>().ToString())
+            .With(x =>
+            x.Password,
+            fixture.Create<string>())
+            );
+
+            fixture.Customize<CreateShortUrlDto>(composer =>
+            composer
+            .With(x =>
+            x.OriginalUrl,
+            fixture.Create<string>())
+            );
+
+            fixture.Customize<AuthRequestDto>(composer =>
+            composer
+            .With(x =>
+            x.Email,
+            fixture.Create<MailAddress>().ToString())
+            .With(x =>
+            x.Password,
+            fixture.Create<string>())
+            );
+        }
+    }
+}
