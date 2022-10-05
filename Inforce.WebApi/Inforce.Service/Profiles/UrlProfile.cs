491742b403d01dfdd42876377b24d2f235a7c75b
Mark14444 <mih.mark2015@gmail.com>
2022-10-05 11:25:17 +0300

    Added profiles for mapping dto`s to entities

---
+++ Inforce.WebApi/Inforce.Service/Profiles/UrlProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using Inforce.Domain.Entities;
+using Inforce.Service.Dto.UrlDtos;
+
+namespace Inforce.Service.Profiles
+{
+    public class UrlProfile:Profile
+    {
+        public UrlProfile()
+        {
+            CreateMap<Url, UrlDto>();
+            CreateMap<Url, UrlDto>().ReverseMap();
+        }
+    }
+}
---
+++ Inforce.WebApi/Inforce.Service/Profiles/UserProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using Inforce.Domain.Entities;
+using Inforce.Service.Dto.UserDtos;
+
+namespace Inforce.Service.Profiles
+{
+    public class UserProfile:Profile
+    {
+        public UserProfile() 
+        {
+            CreateMap<User, UserDto>();
+            CreateMap<User, UserDto>().ReverseMap();
+            CreateMap<User, CreateUserDto>().ReverseMap();
+
+            CreateMap<IEnumerable<User>, IEnumerable<UserDto>>();
+        }
+
+    }
+}
