a75f7ebdb9f002571462f1b28a8eb94ca8fda62f
Mark14444 <mih.mark2015@gmail.com>
2022-10-05 11:27:06 +0300

    Create Program.cs

---
+++ Inforce.WebApi/Inforce.WebApi/Program.cs
@@ -0,0 +1,48 @@
+using Inforce.Services.Extensions;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.Filters;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddDependencies();
+builder.Services.AddJwtValidation(builder);
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddDb();
+builder.Services.AddValidation();
+builder.Services.AddAutoMapper();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
+    {
+        Description = "Standard Authorization header using the Bearer scheme (\"bearer {token}\")",
+        In = ParameterLocation.Header,
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey
+    });
+
+    options.OperationFilter<SecurityRequirementsOperationFilter>();
+});
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
