f0304195a61f4b73a4f6176c1d1b0ae8a0459fa7
Mark14444 <mih.mark2015@gmail.com>
2022-10-05 11:27:21 +0300

    Added controllers

---
+++ Inforce.WebApi/Inforce.WebApi/Controllers/UrlController.cs
@@ -0,0 +1,49 @@
+﻿using Inforce.Service.Dto.UrlDtos;
+using Inforce.Service.Services.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Inforce.WebApi.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UrlController : ControllerBase
+    {
+        private readonly IUrlService _service;
+        public UrlController(IUrlService service)
+        {
+            _service = service;
+        }
+        [HttpGet("get-by-id")]
+        public async Task<IActionResult> GetUrlById(int id)
+        {
+            var url = await _service.GetUrlByIdAsync(id);
+            return Ok(url);
+        }
+        [HttpGet("get-by-short-url")]
+        public async Task<IActionResult> GetUrlByShortUrl(string shortUrl)
+        {
+            var url = await _service.GetUrlByShortName(shortUrl);
+            if(url == null) return NotFound();
+            return Ok(url);
+        }
+        [HttpPost("create")]
+        public async Task<IActionResult> CreateShortUrl(CreateShortUrlDto url)
+        {
+            await _service.AddAsync(url);
+            return Ok();
+        }
+        [HttpGet("get-all")]
+        public async Task<IActionResult> GetAllUrls()
+        {
+            var urls = await _service.GetAllUrlsAsync();
+            return Ok(urls);
+        }
+        [HttpDelete("delete")]
+        public async Task<IActionResult> DeleteUrl(int id)
+        {
+            return await _service.DeleteAsync(id) ? Ok():BadRequest();
+        }
+    }
+}
---
+++ Inforce.WebApi/Inforce.WebApi/Controllers/UserController.cs
@@ -0,0 +1,60 @@
+﻿using Inforce.Service.Dto.AuthorizationDtos;
+using Inforce.Service.Dto.UserDtos;
+using Inforce.Service.Services.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Inforce.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserService _service;
+        public UserController(IUserService service)
+        {
+            _service = service;
+        }
+        [HttpGet("get-me"), Authorize]
+        public async Task<IActionResult> GetMe()
+        {
+            var userName = await _service.GetMeAsync();
+            return Ok(userName);
+        }
+        [HttpPost("register")]
+        public async Task<IActionResult> RegiserUser(CreateUserDto user)
+        {
+            return await _service.AddAsync(user) ? Ok() : BadRequest();
+        }
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(AuthRequestDto user)
+        {
+            var response = await _service.LoginAsync(user);
+            if (response == null)
+                return BadRequest(new { message = "Username or password is incorrect" });
+            return Ok(response);
+        }
+        [HttpGet("get-all"), Authorize]
+        public async Task<IActionResult> GetAllUsers()
+        {
+            var users = await _service.GetAllAsync();
+            return Ok(users);
+        }
+        [HttpGet("get-by-id"), Authorize]
+        public async Task<IActionResult> GetUserById(int id)
+        {
+            var user = await _service.GetByIdAsync(id);
+            return Ok(user);
+        }
+        [HttpDelete("delete"), Authorize]
+        public async Task<IActionResult> RemoveUser(int id)
+        {
+            return await _service.RemoveAsync(id) ? Ok() : BadRequest();
+        }
+        [HttpPut("update"), Authorize]
+        public async Task<IActionResult> UpdateUser(UserDto entity)
+        {
+            return await _service.UpdateAsync(entity) ? Ok() : BadRequest();
+        }
+    }
+}
