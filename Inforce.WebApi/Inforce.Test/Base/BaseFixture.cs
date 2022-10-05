3c3de8316ba61b4b5a4ec96c1358526842b5afb2
Mark14444 <mih.mark2015@gmail.com>
2022-10-05 11:29:47 +0300

    Created BaseFixture class for unit testing

---
+++ Inforce.WebApi/Inforce.Test/Base/BaseFixture.cs
@@ -0,0 +1,28 @@
+﻿using AutoFixture;
+using AutoMapper;
+using Inforce.Service.Profiles;
+using System.Collections.Generic;
+
+namespace Fragments.Test.Base
+{
+    public class BaseFixture
+    {
+        protected Fixture Fixture { get; set; }
+
+        protected IMapper Mapper { get; set; }
+
+        public BaseFixture()
+        {
+            Fixture = new Fixture();
+            Fixture.Behaviors.Remove(new ThrowingRecursionBehavior());
+            Fixture.Behaviors.Add(new OmitOnRecursionBehavior());
+
+            Mapper = new Mapper(new MapperConfiguration(options =>
+                options.AddProfiles(new List<Profile>
+                    {
+                        new UserProfile(),
+                        new UrlProfile()
+                    })));
+        }
+    }
+}
