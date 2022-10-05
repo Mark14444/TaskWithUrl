0df6622f68238f3b32a2abc376e80c9653caa739
Mark14444 <mih.mark2015@gmail.com>
2022-10-05 11:30:05 +0300

    InMemory database for testing

---
+++ Inforce.WebApi/Inforce.Test/Base/ContextGenerator.cs
@@ -0,0 +1,18 @@
+﻿using Inforce.Repository;
+using Microsoft.EntityFrameworkCore;
+
+namespace Fragments.Test.Base
+{
+    internal static class ContextGenerator
+    {
+        internal static InforceContext GetContext()
+        {
+            var contextOptions = new DbContextOptionsBuilder<InforceContext>()
+            .UseInMemoryDatabase(databaseName: "InforceDb")
+            .Options;
+
+            return new InforceContext(contextOptions);
+        }
+    }
+}
+
