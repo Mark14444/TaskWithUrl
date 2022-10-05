c6449bea615fb1f0fcd95704bdc4cad996ef19b3
Mark14444 <mih.mark2015@gmail.com>
2022-10-05 11:22:45 +0300

    Added configuration for relations between tables and constraints

---
+++ Inforce.WebApi/Inforce.Repository/TableConfigurations/UrlConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using Inforce.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Inforce.Repository.TableConfigurations
+{
+    public class UrlConfiguration : IEntityTypeConfiguration<Url>
+    {
+        public void Configure(EntityTypeBuilder<Url> builder)
+        {
+            builder.HasKey(x => x.Id);
+        }
+    }
+}
---
+++ Inforce.WebApi/Inforce.Repository/TableConfigurations/UserConfiguration.cs
@@ -0,0 +1,24 @@
+﻿
+using Inforce.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Inforce.Repository.TableConfigurations
+{
+    public class UserConfiguration : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.HasKey(x => x.Id);
+
+            builder.HasMany(x => x.Urls)
+                .WithOne(x => x.User)
+                .HasForeignKey(x => x.UserId);
+
+            builder.HasIndex(x => x.Email).IsUnique();
+
+            builder.Property(u => u.Role)
+            .HasConversion<string>();
+        }
+    }
+}
